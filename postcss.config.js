9221a88699663654c903c9b390863247dac341db
Nicholas Echols <stere.ereo@gmail.com>
2019-10-16 21:50:51 -0700

    css minimizers

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+const purgecssPlugin = purgecss({
+  content: ['./client/src/**/*.pug', './client/src/**/*.js'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
 
 module.exports = {
-  plugins: [tailwindcss('./tailwind.config.js'), autoprefixer]
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    ...(process.env.NODE_ENV === 'production' ? [purgecssPlugin] : [])
+  ]
 };
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,9 +1,13 @@
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
     moduleIds: 'hashed',
     runtimeChunk: 'single',
     splitChunks: {
